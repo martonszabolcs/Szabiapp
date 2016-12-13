9b659a019a1b68925a3926ae465214dda95a8a99
Szabolcs Marton <martar1994@gmail.com>
2016-12-13 16:22:47 +0100

    View Style

--- index.ios.js
+++ index.ios.js
@@ -8,29 +8,59 @@ import React, { Component } from 'react';
 import {
   AppRegistry,
   StyleSheet,
+  Alert,
   Text,
+  Button,
   View
 } from 'react-native';
 
+const onButtonPress = () => {
+  Alert.alert('Király');
+};
+
+
 var Szabiapp = React.createClass({
   render: function () {
-    return  <View style={[styles.buttonWrapper]}>
-        {this.FirstButton()}
-       </View>
-    },
+  return <View style={styles.container}>
 
-  FirstButton: function () {
-    return
-      <View style={[styles.buttonWrapper]}>
-        <Text>
-        143423423wgergerbetb4232fsbdetger
-        </Text>
-      </View>
+  <View style={styles.header}>
+
+
+  <View style={[styles.footer]}>
+          {this.firstButton()}
+
+  <View style={[styles.button]}>
+          {this.circle()}
+
+       </View>
+       </View>
+       </View>
+       </View>
+},
+
+  firstButton: function () {
+    return (
+      <Button
+  onPress={onButtonPress}
+  title="1"
+  color="#DD0000"
+  accessibilityLabel="Learn more about this purple button"
+/>
+
+      )
+      
   },
 
   SecondButton: function(){
-    return
+    return (
+      <Button
+  onPress={onButtonPress}
+  title="2"
+  color="#DD0000"
+  accessibilityLabel="Learn more about this purple button"
+/>
 
+      )
   },
 
   ThirdButton: function(){
@@ -50,28 +80,62 @@ var Szabiapp = React.createClass({
     return
   },
 
-  Circle: function(){
+  circle: function(){
     return 
+    <View style={styles.button}>
+    </View>
   }
 });
 
 
   var styles = StyleSheet.create({
 
-    Circle: {
+    circle: {
       borderColor: '#00CC00',
       justifyContent: 'center',
       alignItems: 'center',
     },
+
+     header: {
+      flex: 1,
+      },
+    footer: {
+      flex:1,
+      borderWidth: 2,
+      height: 100,
+      borderRadius: 50,
+      flexDirection: 'row',
+      justifyContent: 'center',
+      alignItems: 'center'
+      },
+
+    lapText: {
+    fontSize: 30
+    },
+    
     FirstButton: {
     
     },
 
+    button: {
+      borderWidth: 2,
+      height: 100,
+      width: 100,
+      borderRadius: 50,
+      justifyContent: 'space-around',
+      alignItems: 'center',
+      borderColor: '#CC0000'
+    },
+
     buttonWrapper: {
-    flex: 3,
-    flexDirection: 'row',
-    justifyContent: 'space-around',
-    alignItems: 'center'
+      flex: 3,
+      flexDirection: 'row',
+      justifyContent: 'space-around',
+      alignItems: 'center'
+    },
+    container: {
+    flex: 1,
+    alignItems: 'stretch'
     },
 
     SecondButton: {
